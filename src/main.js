4f3cca7c01b5df8aa398c0cb5b7e279e6be14d31
Andrew Wang <andrew_wang2002@hotmail.com>
2023-03-09 13:45:06 -0500

    UPD -> latest changes to gitignore

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
 import { createApp } from 'vue'
 import './style.css'
 
-// PrimeVue 
+// PrimeVue & PrimeIcon
 import PrimeVue from 'primevue/config';
-import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primevue/resources/themes/lara-dark-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 
 import App from './App.vue'
 
+// PrimeVue Components
+import Button from 'primevue/button';
+
 const app = createApp(App);
 
 app.use(PrimeVue, { ripple: true });
+app.component("Button", Button);
 
 app.mount('#app');
