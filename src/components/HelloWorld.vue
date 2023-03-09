bef81d42fed072d5ae135b69b33415324c715afb
Andrew Wang <andrew_wang2002@hotmail.com>
2023-03-09 14:15:59 -0500

    UPD -> add router and services

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,19 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
+import Button from 'primevue/button';
 
 defineProps({
   msg: String,
-})
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <Button type="button" @click="count++">count is {{ count }}</Button>
+    <Button label="button" @click="count++">count is {{ count }}</Button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import './style.css';
+
+// Personal Views
+import App from './App.vue';
+import HelloWorld from './components/HelloWorld.vue';
 
 // PrimeVue & PrimeIcon
 import PrimeVue from 'primevue/config';
@@ -7,14 +12,34 @@ import "primevue/resources/themes/lara-dark-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 
-import App from './App.vue'
-
 // PrimeVue Components
 import Button from 'primevue/button';
 
+// PrimeVue Services
+import ConfirmationService from "primevue/confirmationservice";
+import ToastService from "primevue/toastservice";
+import DialogService from "primevue/dialogservice";
+
 const app = createApp(App);
 
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: HelloWorld,
+            props: { msg: "Hello Sussy" }
+        },
+    ],
+});
+
+app.use(router);
 app.use(PrimeVue, { ripple: true });
+app.use(ConfirmationService);
+app.use(ToastService);
+app.use(DialogService);
+
 app.component("Button", Button);
 
 app.mount('#app');
