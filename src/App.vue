4155f38ac5914a44b6a94a933c590307740703d4
Andrew Wang <andrew_wang2002@hotmail.com>
2024-06-22 22:12:19 -0600

    update menus

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,30 @@
 <script setup>
+import { ref } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
 import { RouterView } from 'vue-router';
 import Toast from 'primevue/toast';
 
 import Menubar from 'primevue/menubar';
+
+const menus = ref([
+  {
+    label: 'Home',
+    icon: 'pi pi-home'
+  },
+  {
+      label: 'Contact',
+      icon: 'pi pi-envelope'
+  }
+]);
 </script>
 
 <template>
   <div>
-    <Menubar :model="[
-          {
-              label: 'Home',
-              icon: 'pi pi-home'
-          },
-          {
-              label: 'Features',
-              icon: 'pi pi-star'
-          },
-          {
-              label: 'Projects',
-              icon: 'pi pi-search',
-              items: [
-                  {
-                      label: 'Components',
-                      icon: 'pi pi-bolt'
-                  },
-                  {
-                      label: 'Blocks',
-                      icon: 'pi pi-server'
-                  },
-                  {
-                      label: 'UI Kit',
-                      icon: 'pi pi-pencil'
-                  },
-                  {
-                      label: 'Templates',
-                      icon: 'pi pi-palette',
-                      items: [
-                          {
-                              label: 'Apollo',
-                              icon: 'pi pi-palette'
-                          },
-                          {
-                              label: 'Ultima',
-                              icon: 'pi pi-palette'
-                          }
-                      ]
-                  }
-              ]
-          },
-          {
-              label: 'Contact',
-              icon: 'pi pi-envelope'
-          }
-      ]">
-        <template #start>
-          <img class="logo" src="/src/assets/home.svg" >
-        </template>
-      </Menubar>
+    <Menubar :model="menus">
+      <template #start>
+        <img class="logo" src="/src/assets/home.svg" >
+      </template>
+    </Menubar>
   </div>
   <RouterView />
   <Toast />
