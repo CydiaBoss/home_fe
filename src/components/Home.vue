3f359f55f904c365a96d2b8f8e7c4ffffbd3ca7e
Andrew Wang <andrew_wang2002@hotmail.com>
2024-06-23 15:03:06 -0600

    More clean up

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,30 +1,12 @@
 <script setup>
-import { ref } from 'vue';
-import Button from 'primevue/button';
-import Paginator from 'primevue/paginator';
-
-defineProps({
-  msg: String,
-});
-
-const count = ref(0);
 </script>
 
 <template>
   <h1>{{ $t("appTitle") }}</h1>
-
-  <div class="card">
-    <p>
-      {{ $t("homeDescription") }}
-    </p>
-  </div>
-
-  <Paginator :rows="10" :totalRecords="120" :rowsPerPageOptions="[10, 20, 30]"></Paginator>
-
+  <p>
+    {{ $t("homeDescription") }}
+  </p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
 </style>
